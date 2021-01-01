703836ac9373931c52ae0d31f68d90d7edb19d7c
Vladimir <turkonjavlada@gmail.com>
2021-01-01 14:05:13 +0100

    Add current user route

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 import express from 'express';
 import { json } from 'body-parser';
+import { currentUserRouter } from './routes/current-user';
 
 const app = express();
 app.use(json());
+
+app.use(currentUserRouter);
 
 app.listen(3000, () => console.log(`Ready at http://locahost:3000`));
