303e95f2fb4d4e09628be41b2a46fd636876cd97
Vladimir <turkonjavlada@gmail.com>
2021-01-10 18:50:42 +0100

    Update if check

--- src/index.ts
+++ src/index.ts
@@ -34,7 +34,7 @@ app.all('*', async (req, res, next) => {
 app.use(errorHandler);
 
 const start = async () => {
-  if (process.env.JWT_KEY) {
+  if (!process.env.JWT_KEY) {
     throw new Error('JWT_KEY must be defined');
   }
   try {
