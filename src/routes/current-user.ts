4b870925033525874493e3b1cfd5241743372d7b
Vladimir <turkonjavlada@gmail.com>
2021-02-21 18:10:43 +0100

    Add endpoint to fetch the current user

--- src/routes/current-user.ts
+++ src/routes/current-user.ts
@@ -1,8 +1,20 @@
 import express from 'express';
+import jwt from 'jsonwebtoken';
 
 const router = express.Router();
 
 router.get('/api/users/currentuser', (req, res) => {
+  if (!req.session?.jwt) {
+    return res.send({ currentUser: null });
+  }
+
+  try {
+    const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!);
+    res.send({ currentUser: payload });
+  } catch (error) {
+    res.send({ currentUser: null });
+  }
+
   res.send('Current user route');
 });
 
