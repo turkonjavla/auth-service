9020cd1f6e42dee8a798438d8264965059408a44
Vladimir <turkonjavlada@gmail.com>
2021-03-07 17:33:17 +0100

    Add more tests
    Checks if:
    - it's a valid email
    - an invalid password
    - missing email and/or password

--- src/routes/__test__/signup.test.ts
+++ src/routes/__test__/signup.test.ts
@@ -10,3 +10,36 @@ it('returns a 201 on successful signup', async () => {
     })
     .expect(201);
 });
+
+it('returns a 400 with an invalid email', async () => {
+  return request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'invalidemail',
+      password: 'password',
+    })
+    .expect(400);
+});
+
+it('returns a 400 with an invalid password', async () => {
+  return request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'test@test.com',
+      password: 'a',
+    })
+    .expect(400);
+});
+
+it('returns a 400 with a missing email and password', async () => {
+  await request(app)
+    .post('/api/users/signup')
+    .send({ email: 'test@test.com' })
+    .expect(400);
+  await request(app)
+    .post('/api/users/signup')
+    .send({
+      password: 'password',
+    })
+    .expect(400);
+});
