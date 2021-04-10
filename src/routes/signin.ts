1c3c1104540ff4c12f284fce239b8ff82ebd4948
Vladimir <turkonjavlada@gmail.com>
2021-04-10 14:15:37 +0200

    Remove errors and middlewares  in favor of using the common package

--- src/routes/signin.ts
+++ src/routes/signin.ts
@@ -1,8 +1,7 @@
 import express, { Request, Response } from 'express';
 import { User } from '../models/user';
 import { body } from 'express-validator';
-import { validateRequest } from '../middlewares/validate-request';
-import { BadRequestError } from '../errors/bad-request-error';
+import { validateRequest, BadRequestError } from '@vttickets/common';
 import { Password } from '../services/password';
 import jwt from 'jsonwebtoken';
 
--- src/routes/signup.ts
+++ src/routes/signup.ts
@@ -1,9 +1,8 @@
 import express, { Request, Response } from 'express';
 import { body } from 'express-validator';
-import { BadRequestError } from '../errors/bad-request-error';
+import { BadRequestError, validateRequest } from '@vttickets/common';
 import jwt from 'jsonwebtoken';
 import { User } from '../models/user';
-import { validateRequest } from '../middlewares/validate-request';
 
 const router = express.Router();
 
